972a50f0c565a5722fa8f3390299ce6b3b3357f9
Mike <mike.capozzoli@gmail.com>
2013-02-25 18:00:53 -0500

    more things!

--- R_motor_control_based_on_IMU_data/R_motor_control_based_on_IMU_data.ino
+++ R_motor_control_based_on_IMU_data/R_motor_control_based_on_IMU_data.ino
@@ -12,10 +12,8 @@
 
 float parsed_IMU_data[3];                      // float array for storing the parsed IMU data.
 String IMU_input_string = "";                  // String to store the incoming data from the IMU
-String roll_string = "";                       // String to fill with the roll data
-String pitch_string = "";                      // String to fill with the pitch data
-String yaw_string = "";                        // String to fill with the yaw data
 
+// motor pin objects
 #define motor1_pin 2
 #define motor2_pin 3
 #define motor3_pin 4
@@ -34,12 +32,19 @@ Servo motor6;
 // Initialize motor speeds to neutral
 int motor1_speed = 1500;
 int motor2_speed = 1500;
+int motor3_speed = 1500;
+int motor4_speed = 1500;
+int motor5_speed = 1500;
+int motor6_speed = 1500;
+
+// loop tracking variables
 unsigned long last_time = 0;
 unsigned long loop_runs = 0;
 
+// status tracking variables
 boolean stopped = true;
 boolean armed = false;
-boolean stringComplete = false;
+boolean string_complete = false;
 boolean ready_for_data = true;
 
 #define DEBUGGING 1                          // used to print debugging messages
@@ -48,9 +53,9 @@ void setup() {
   Serial.begin(38400);
   Serial1.begin(38400);
   IMU_input_string.reserve(64);
-  roll_string.reserve(10);
-  pitch_string.reserve(10);
-  yaw_string.reserve(10);
+//  roll_string.reserve(10);
+//  pitch_string.reserve(10);
+//  yaw_string.reserve(10);
   
   motor_setup();
   #if DEBUGGING == 1
@@ -66,9 +71,9 @@ void loop() {
   while( millis()- last_time > 300) {
     last_time = millis();
     // IMU data retrieved and transformed to make it useful for the Arduino
-    get_IMU_data(parsed_IMU_data);
+    get_IMU_data();
     // Motor adjustment based on IMU input
-    motor_driver(stopped, armed, parsed_IMU_data);
+    motor_driver(stopped, armed);
     Serial1.flush();
   }
 }
@@ -78,40 +83,32 @@ void loop() {
   Input: 
   Output: 
   *******************************************************************/
-void get_IMU_data(float* IMU_data) {
-  // colons, commas, and the star are used to mark where each piece of data may be found
-  int colon_1;
-  int colon_2;
-  int colon_3;
-  int comma_1;
-  int comma_2;
-  int star;
-  
+void get_IMU_data() {
   // The if statement provides a condition for running the conversion. We
   // only want to run the conversion every half second in order to not be
   // swtiching the motor speeds constantly
-  if( stringComplete == true) {
+  if( string_complete == true) {
       // Next 6 statements are used to mark indeces of the delimiter characters
-      colon_1 = IMU_input_string.indexOf(':');
-      comma_1 = IMU_input_string.indexOf(',');
+      int colon_1 = IMU_input_string.indexOf(':');
+      int comma_1 = IMU_input_string.indexOf(',');
       
-      colon_2 = IMU_input_string.indexOf(':',colon_1 + 1);
-      comma_2 = IMU_input_string.indexOf(',',comma_1+1);
+      int colon_2 = IMU_input_string.indexOf(':',colon_1 + 1);
+      int comma_2 = IMU_input_string.indexOf(',',comma_1+1);
       
-      colon_3 = IMU_input_string.indexOf(':',colon_2 + 1);
-      star = IMU_input_string.indexOf('*');
+      int colon_3 = IMU_input_string.indexOf(':',colon_2 + 1);
+      int star = IMU_input_string.indexOf('*');
       
-      roll_string = IMU_input_string.substring(colon_1+1,comma_1);
-      pitch_string = IMU_input_string.substring(colon_2+1,comma_2);
-      yaw_string = IMU_input_string.substring(colon_3+1,star);
+      String roll_string = IMU_input_string.substring(colon_1+1,comma_1);
+      String pitch_string = IMU_input_string.substring(colon_2+1,comma_2);
+      String yaw_string = IMU_input_string.substring(colon_3+1,star);
       
       // Conversion from strings to floating point numbers
-      IMU_data[0] = roll_string.toFloat();
-      IMU_data[1] = pitch_string.toFloat();
-      IMU_data[2] = yaw_string.toFloat();
+      parsed_IMU_data[0] = roll_string.toFloat();
+      parsed_IMU_data[1] = pitch_string.toFloat();
+      parsed_IMU_data[2] = yaw_string.toFloat();
       
       IMU_input_string = "";
-      stringComplete = false;
+      string_complete = false;
       ready_for_data = true;
       
       #if DEBUGGING == 1
@@ -132,11 +129,11 @@ void get_IMU_data(float* IMU_data) {
         
         Serial.println("Floats");
         Serial.print("RLL:");
-        Serial.print(IMU_data[0]);
+        Serial.print(parsed_IMU_data[0]);
         Serial.print(" PCH:");
-        Serial.print(IMU_data[1]);
+        Serial.print(parsed_IMU_data[1]);
         Serial.print(" YAW: ");
-        Serial.println(IMU_data[2]);
+        Serial.println(parsed_IMU_data[2]);
       #endif
     }  
 }
@@ -151,7 +148,11 @@ void get_IMU_data(float* IMU_data) {
 boolean motor_setup() {
   motor1.attach(motor1_pin);
   motor2.attach(motor2_pin);
-  return motor1.attached() && motor2.attached();
+  motor3.attach(motor3_pin);
+  motor4.attach(motor4_pin);
+  motor5.attach(motor5_pin);
+  motor6.attach(motor6_pin);
+  return motor1.attached() && motor2.attached() && motor3.attached() && motor4.attached() && motor5.attached() && motor6.attached();
 }
 /****************************************************************
   End Motor Module
@@ -159,13 +160,11 @@ boolean motor_setup() {
 
 /****************************************************************
   Motor Driver Module
-  Input: Booleans - state of the servo motor
+  Input: Booleans - states of the servo motors
   Output: void
   This function starts and stops each motor according to input
 ****************************************************************/
-void motor_driver(boolean& temp_stopped, boolean& temp_armed, float* IMU_data) {
-  // base_value is our test value for the sensors
-  float base_value = 0.0;
+void motor_driver(boolean& temp_stopped, boolean& temp_armed) {
   // storage variables which will be used to scale the motor speeds
   float current_roll;
   float current_pitch;
@@ -180,21 +179,33 @@ void motor_driver(boolean& temp_stopped, boolean& temp_armed, float* IMU_data) {
     }
   }
   if(temp_armed && !temp_stopped) {
-    // current role must be calculated to determine how much the motor
-    // must compensate
-    current_roll = base_value - IMU_data[0];
+    // current role must be calculated to determine how much motors
+    // 4 and 5 must compensate
+    current_roll = parsed_IMU_data[0];
+    
+    // INSERT STATEMENT HERE THAT ADDS USER INPUT VARIABLE
+    // AND ADJUSTS THE ROLL VALUES APPROPRIATELY
+    
 
-    int m1; 
-    m1 = int(current_roll);
+    // roll must be converted to an integer in order to be used in
+    // a map command
+    int current_roll_integer = int(current_roll);
     
-    m1 = constrain(m1, -180, 180);
-    motor1_speed = map(m1, -180, 180, 1300, 1700);
-    motor2_speed = map(m1, 180, -180, 1300, 1700);
+    // for safety and because unpredictable inputs are always a 
+    // factor the roll integer is constrained to a usable range
+    current_roll_integer = constrain(current_roll_integer, -180, 180);
     
-    Serial.print("Servo Speed: ");
+    // roll values get mapped to usable ranges for the Servos
+    motor1_speed = map(current_roll_integer, -180, 180, 1300, 1700);
+    motor2_speed = map(current_roll_integer, 180, -180, 1300, 1700);
+    
+    Serial.print("Servo Speeds: ");
     Serial.print(motor1_speed);
     Serial.print(" , ");
     Serial.println(motor2_speed);
+    
+    // if statement used to create a dead zone so that the sub
+    // doesn't continuously bounce back and forth
     if( motor1_speed > 1490 && motor1_speed < 1510) {
       motor1.writeMicroseconds(1500);
       motor2.writeMicroseconds(1500);
@@ -256,7 +267,7 @@ void serialEvent1() {
         char in_char = Serial1.read();
         IMU_input_string += in_char;
         if (in_char == '*') {
-          stringComplete = true;
+          string_complete = true;
           ready_for_data = false;
           first_char = '*';
         }
